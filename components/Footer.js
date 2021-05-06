eea26a43f289bee72935195228a02f4c5d6eea08
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-05-06 22:46:20 +0200

    next basics

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+
+import styles from '../styles/Home.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.center}>
+            Made with Love
+
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css'
+
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+
+      <section>
+        About
+            </section>
+
+    </div>
+  );
+}
+
+export default About;
